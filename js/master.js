4ae8b69d1a8d8c3a75e435f6519b62862146855c
lindasidauy11 <linda94sid@gmail.com>
2018-04-25 11:40:33 -0500

    cambio de cintact

--- js/master.js
+++ js/master.js
@@ -1,4 +1,48 @@
-$(document).ready(function() {
+// Input Lock
+$('textarea').blur(function () {
+    $('#hire textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:first-child input').blur(function () {
+    $('#hire .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:nth-child(2) input').blur(function () {
+    $('#hire .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+
+/*$(document).ready(function() {
 	// Test for placeholder support
     $.support.placeholder = (function(){
         var i = document.createElement('input');
@@ -56,7 +100,7 @@ $(document).ready(function() {
                 if( $this.val() !== '' ) {
                     $parent.removeClass('js-unhighlight-label');
                 }
-            }*/
+            }
         });
     }
-});
+});*/
